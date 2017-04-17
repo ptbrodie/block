80c02938a5e5b68df1b111a75cfc66e4ec22fe36
Patrick Brodie <patrick@shoptiques.com>
2017-04-16 20:41:38 -0400

    basic block setup: add, remove, list

--- block.sh
+++ block.sh
@@ -2,18 +2,61 @@
 
 BLOCK_MARKER="ADDED BY BLOCK"
 
-function _unblock() {
+function strip_www() {
+    echo -e $1 | sed 's/www\.//g'
+}
+
+function strip_www_read {
+    while read host; do
+        strip_www $host
+    done
+}
+
+function remove() {
     set -e
-    COMMAND="touch /etc/tmp_hosts; sed -i '' '/$1/ s/^.*$BLOCK_MARKER//' /etc/hosts >> /etc/tmp_hosts; mv /etc/tmp_hosts /etc/hosts"
-    sudo sh -c "$COMMAND"
+    bare=`strip_www $1`
+    sudo touch /etc/tmp_hosts
+    regex="$bare.*$BLOCK_MARKER"
+    sudo grep -v "$regex" /etc/hosts >> /etc/tmp_hosts
+    sudo mv /etc/tmp_hosts /etc/hosts
 }
 
-function _block() {
-    echo "127.0.0.1    $1    # $BLOCK_MARKER" >> /etc/hosts
+function add() {
+    bare=`strip_www $1`
+    www="www.$bare"
+    echo "127.0.0.1     $bare   # $BLOCK_MARKER" >> /etc/hosts
+    echo "127.0.0.1     $www    # $BLOCK_MARKER" >> /etc/hosts
+    echo "fe80::1%lo0   $bare   # $BLOCK_MARKER" >> /etc/hosts
+    echo "fe80::1%lo0   $www    # $BLOCK_MARKER" >> /etc/hosts
 }
 
-if [ $1 == 'block' ]; then
-    _block $2
-elif [ $1 == 'unblock' ]; then
-    _unblock $2
+function list() {
+    sudo grep "$BLOCK_MARKER" /etc/hosts | awk '{print $2}' | strip_www_read | sort | uniq
+}
+
+function help() {
+    echo "usage: block [add|remove|list] <arg>"
+    echo
+    echo "To block a website:"
+    echo "    $ block add example.com"
+    echo
+    echo "To unblock a website:"
+    echo "    $ block remove example.com"
+    echo
+    echo "View currently blocked websites:"
+    echo "    $ block list"
+}
+
+if [ $1 == 'add' ]; then
+    add $2
+elif [ $1 == 'remove' ]; then
+    remove $2
+elif [ $1 == 'list' ]; then
+    list
+elif [ $1 == 'help' ]; then
+    help
+else
+    echo "$1 not recognized."
+    echo "More info:"
+    echo "    $ block help"
 fi
